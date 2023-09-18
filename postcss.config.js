474725a205adcdf0700482e6ae25dcbf4eee005b
Asiye <116940286+AsiyeZBedir@users.noreply.github.com>
2023-09-18 13:12:51 +0300

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+// tailwindcss modülünü içeri aktarıyoruz
+const tailwindcss = require('tailwindcss');
+
+// PostCSS yapılandırma nesnesini dışa aktarıyoruz
+module.exports = {
+  // PostCSS eklentilerini belirtiyoruz
+  plugins: [
+    // Tailwind CSS eklentisini ekliyoruz ve tailwind.js konfigürasyon dosyasını belirtiyoruz
+    tailwindcss('./tailwind.js'),
+    
+    // Autoprefixer eklentisini ekliyoruz
+    require('autoprefixer')
+  ]
+}
